abff3bd2c3c1c3af0aa04f47437fe04e68e87182
khujamov1 <elyorkhujamov4@gmail.com>
2024-02-10 21:35:16 +0500

    Correct some issues

--- src/pages/resume/pages/skills/Skills.jsx
+++ src/pages/resume/pages/skills/Skills.jsx
@@ -1,9 +1,8 @@
-import ApexCharts from "apexcharts";
 import { ApexChart } from "../../../../components/charts/Charts";
 
 export const Skills = () => {
 	return (
-		<div className="w-[400px]">
+		<div className="md:w-[400px] max-md:max-w-[300px]">
 			<h3 className="text-center font-bold  text-3xl text-orange-600 mb-5">
 				Skills
 			</h3>
